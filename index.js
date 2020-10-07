000f1ad3c1a4840bfb97b3b6940962112a64a2f0
George Smith <georgesmith812@gmail.com>
2020-10-07 17:32:29 -0400

    modified some of the html generating functions and created global store value

--- index.js
+++ index.js
@@ -1,31 +1,35 @@
 "use strict";
 
-//Global stored values (Would like to create a global state object to store these in..)
-const apiKeyRapid = "583ca639f9msh2485b7c56c251aap137d5bjsn5fb3b724e00f";
-const apiKeyZom = "af7764942e15c7d2bff6391d65fd21c8";
-let coordinates = [];
-let locationId = "";
-let locationName = "";
+//Global stored values
+let store = {
+    coordinates : [],
+    locationId : "",
+    locationName : "",
+    apiKeyRapid : "583ca639f9msh2485b7c56c251aap137d5bjsn5fb3b724e00f"
+}
 //Display functions
 
 //Displays dynamically the description and name of the place inputted by the user
 //Shows the other buttons required to navigate to useful information
 function displayLocation(response) {
     const locationData = response.data[0].result_object;
-    coordinates.push(locationData.latitude, locationData.longitude);
-    locationId = locationData.location_id;
-    locationName = locationData.name;
+    store.coordinates.push(locationData.latitude, locationData.longitude);
+    store.locationId = locationData.location_id;
+    store.locationName = locationData.name;
+    let locationImg = locationData.photo.images.original.url
+    console.log(store);
     $("#location-description").empty();
     $("#results").empty();
     $("#location-description").append(
         `
-        <h2>${locationName}</h2>
-        <img src="${locationData.photo.images.large.url}" alt="Picture of ${locationData.name}">
+        <img src="${locationImg}" alt="${locationData.photo.caption}">
+        <h2>${store.locationName}</h2>
         <p>${locationData.geo_description}</p>
-        <p>What information would you like to find for ${locationData.name} (Choose one of the following):</p>
-        <button type="button" id="restaurants">Restaurants</button>
-        <button type="button" id="things-to-do">Things to Do</button>
-        <button type="button" id="accommodations">Accommodations</button>
+        <div class="initial-buttons">
+            <button type="button" id="restaurants">Restaurants</button>
+            <button type="button" id="things-to-do">Things to Do</button>
+            <button type="button" id="accommodations">Accommodations</button>
+        </div>
         `
     );
     $("#location-description").removeClass("hidden");
@@ -34,7 +38,6 @@ function displayLocation(response) {
 //Displays a list of restaurants when the corresponding button is pressed
 function displayRestaurants(response) {
     $("#results").empty();
-    $("#results").append(`<h2>List of Restaurants nearby ${locationName}</h2>`);
     for(let i = 0; i < response.data.length; i++) {
         let restaurant = response.data[i];
         if(restaurant.photo === undefined || restaurant.description === "" || restaurant.price_level === "") {
@@ -44,12 +47,12 @@ function displayRestaurants(response) {
             $("#results").append(
                 `
                 <div class="results-item">
-                    <h4>Name of Restaurant: ${restaurant.name}</h4>
+                    <h4>${restaurant.name}</h4>
                     <img src="${restaurant.photo.images.medium.url}" alt="${restaurant.photo.caption}">
                     <p>${restaurant.description}</p>
-                    <p>Price Level: ${restaurant.price_level}</p>
-                    <p>Address: ${restaurant.address}</p>
-                    <p>Rating: ${restaurant.rating} out of 5</p>
+                    <p><b>Price Level:</b> ${restaurant.price_level}</p>
+                    <p><b>Address:</b> ${restaurant.address}</p>
+                    <p><b>Rating:</b> ${restaurant.rating} out of 5</p>
                     <p><a href="${restaurant.website}" target="_blank">Website</a></p>
                 </div>
                 `
@@ -63,7 +66,6 @@ function displayRestaurants(response) {
 //Function controls displaying the html dynamically after the things to button is pressed
 function displayThingsToDo(response) {
     $("#results").empty();
-    $("#results").append(`<h2>List of top Things to Do in ${locationName}</h2>`);
     for(let i = 0; i < response.data.length; i++) {
         if(response.data[i].location_id > 34515 && response.data[i].description !== "" && response.data[i].photo !== undefined) {
             $("#results").append(
@@ -72,8 +74,8 @@ function displayThingsToDo(response) {
                     <h4>${response.data[i].name}</h4>
                     <img src="${response.data[i].photo.images.medium.url}" alt="${response.data[i].photo.caption}">
                     <p>${response.data[i].description}<p>
-                    <p>Address: ${response.data[i].address}</p>
-                    <p>Phone Number: ${response.data[i].phone}</p>
+                    <p><b>Address:</b> ${response.data[i].address}</p>
+                    <p><b>Phone Number:</b> ${response.data[i].phone}</p>
                     <p><a href="${response.data[i].website}" target="_blank">Website</a></p>
                 </div>
                 `
@@ -87,6 +89,7 @@ function displayThingsToDo(response) {
     $("#results").removeClass("hidden");
 }
 
+//Creates the Form necessary to grab stay information from user
 function createLengthOfStayForm(data) {
     const destinationId = data.suggestions[0].entities[0].destinationId
     const currentDate = new Date();
@@ -94,7 +97,6 @@ function createLengthOfStayForm(data) {
     $("#results").append(
         `
         <div class="results-form">
-            <h3>Just need a little more information...</h3>
             <form id="length-of-stay">
                 <label for="check-in">Check-In Date:</label> 
                 <input type="date" id="check-in" class="checkIn" value="${currentDate}" min="${currentDate}" required>
@@ -102,11 +104,10 @@ function createLengthOfStayForm(data) {
                 <input type="date" id="check-out" class="checkOut" value="${currentDate}" min="${currentDate}" required>
                 <label for="adults">Number of Adults:</label>
                 <input type="number" id="adults" class="numberAdults" value="2" min="0" max="8" required>
-                <button type="submit" id="js-hotels-submit">Search for Places</button>
+                <button type="submit" id="js-hotels-submit">Search Places</button>
             </form>
         </div>
         <div id="results-list">
-            <h3>List of places to stay:</h3>
         </div>
         `
     )
@@ -114,6 +115,7 @@ function createLengthOfStayForm(data) {
     handleHotelsButton(destinationId);
 }
 
+//After user has inputted necessary info, displays list of accomodations
 function displayHotelsList(data) {
     if(data.result !== "OK") {
         alert("Search failed!");
@@ -128,7 +130,6 @@ function displayHotelsList(data) {
                     <img src="${resultsList[i].thumbnailUrl}" alt="Thumbnail picture of property">
                     <p>Rating: ${resultsList[i].guestReviews.unformattedRating} out of ${resultsList[i].guestReviews.scale} - ${resultsList[i].guestReviews.badgeText} - From ${resultsList[i].guestReviews.total} Reviews</p>
                     <p>Area: ${resultsList[i].neighbourhood}</p>
-                    <p>Address: ${resultsList[i].address.streetAddress}, ${resultsList[i].address.locality} , ${resultsList[i].address.postalCode}</p>
                     <p>Average Nightly Price: ${resultsList[i].ratePlan.price.current} per night</p>
                 </div>
                 `
@@ -146,7 +147,7 @@ function getDestination(location) {
     const options = {
         headers: new Headers({
             "x-rapidapi-host": "tripadvisor1.p.rapidapi.com",
-            "x-rapidapi-key": apiKeyRapid
+            "x-rapidapi-key": store.apiKeyRapid
         })
     };
     fetch(url, options)
@@ -156,11 +157,11 @@ function getDestination(location) {
 }
 
 function getThingsToDo() {
-    const url = `https://tripadvisor1.p.rapidapi.com/attractions/list?lang=en_US&currency=USD&sort=ranking&lunit=km&limit=20&location_id=${encodeURIComponent(locationId)}`;
+    const url = `https://tripadvisor1.p.rapidapi.com/attractions/list?lang=en_US&currency=USD&sort=ranking&lunit=km&limit=20&location_id=${encodeURIComponent(store.locationId)}`;
     const options = {
         headers: new Headers({
             "x-rapidapi-host": "tripadvisor1.p.rapidapi.com",
-            "x-rapidapi-key": apiKeyRapid
+            "x-rapidapi-key": store.apiKeyRapid
         })
     };
     fetch(url, options)
@@ -170,11 +171,11 @@ function getThingsToDo() {
 } 
 
 function getRestaurants() {
-    const url = `https://tripadvisor1.p.rapidapi.com/restaurants/list?restaurant_tagcategory_standalone=10591&lunit=km&restaurant_tagcategory=10591&limit=30&currency=USD&lang=en_US&location_id=${locationId}`;
+    const url = `https://tripadvisor1.p.rapidapi.com/restaurants/list?restaurant_tagcategory_standalone=10591&lunit=km&restaurant_tagcategory=10591&limit=30&currency=USD&lang=en_US&location_id=${store.locationId}`;
     const options = {
         headers: new Headers({
             "x-rapidapi-host": "tripadvisor1.p.rapidapi.com",
-            "x-rapidapi-key": apiKeyRapid
+            "x-rapidapi-key": store.apiKeyRapid
         })
     };
     fetch(url, options)
@@ -184,11 +185,11 @@ function getRestaurants() {
 }
 
 function getLocationId() {
-    const url = `https://hotels4.p.rapidapi.com/locations/search?locale=en_US&query=${encodeURIComponent(locationName)}`
+    const url = `https://hotels4.p.rapidapi.com/locations/search?locale=en_US&query=${encodeURIComponent(store.locationName)}`
     const options = {
         headers: new Headers({
             "x-rapidapi-host": "hotels4.p.rapidapi.com",
-            "x-rapidapi-key": apiKeyRapid
+            "x-rapidapi-key": store.apiKeyRapid
         })
     };
     fetch(url, options)
@@ -202,7 +203,7 @@ function getHotelsList(checkIn, checkOut, numberAdults, destinationId) {
     const options = {
         headers: new Headers({
             "x-rapidapi-host": "hotels4.p.rapidapi.com",
-            "x-rapidapi-key": apiKeyRapid
+            "x-rapidapi-key": store.apiKeyRapid
         })
     }
     fetch(url, options)
@@ -259,9 +260,9 @@ function handleRestaurantsClicked() {
 
 function handleFindDestination() {
     $("form").submit(event => {
-        coordinates = [];
-        locationId = "";
-        locationName = "";
+        store.coordinates = [];
+        store.locationId = "";
+        store.locationName = "";
         const userInput = $(".js-destination-input").val();
         event.preventDefault();
         getDestination(userInput);
